bb4e5da3e7c5a5e77f1e477feaaf0c762215d2fb
dayron24 <132240836+dayron24@users.noreply.github.com>
2023-06-01 03:15:36 -0600

    Add files via upload

---
+++ DATABASE/tablas_sqlserver.sql
@@ -0,0 +1,156 @@
+CREATE TABLE Usuarios(
+	idUsuario iNT IDENTITY (1,1) PRIMARY KEY,
+	correoElectronico varchar(100),
+	contrasenna varchar(100),
+	rol varchar(100)
+);
+
+CREATE TABLE Profesor (
+    codigo varchar(100) PRIMARY KEY,
+    nombreCompleto varchar(100),
+    correoElectronico varchar(100),
+    telefonoOficina varchar(100),
+    telefonoCelular varchar(100),
+    fotografia varbinary(100),
+    activo VARCHAR(20)
+);
+
+CREATE TABLE EquipoGuia (
+    idEquipoGuia iNT PRIMARY KEY,
+    anno int,
+    idProfesorCoordinador varchar(100) FOREIGN KEY REFERENCES Profesor(codigo),
+    ultimaModificacion varchar(100)
+);
+
+
+CREATE TABLE CentroAcademico (
+    idCentroAcademico iNT PRIMARY KEY,
+    
+    nombre varchar(100),
+    cantProfesores INT,
+    Siglas varchar(20)
+);
+    
+CREATE TABLE Asistente (
+	idAsistente iNT PRIMARY KEY,
+	nombre varchar(100),
+	correo varchar(100),
+	idCentroAcademico int FOREIGN KEY REFERENCES CentroAcademico(idCentroAcademico)
+)
+
+
+CREATE TABLE Estudiante (
+    idEstudiante iNT PRIMARY KEY,
+    carne varchar(100),
+    nombreCompleto varchar(100),
+    correoElectronico varchar(100),
+    telefonoCelular varchar(100),
+    idCentroAcademico int FOREIGN KEY REFERENCES CentroAcademico(idCentroAcademico)
+     
+);
+
+CREATE TABLE EquipoGuiaXprofesores (
+    idEquipoGuiaXprofesores iNT IDENTITY (1,1) PRIMARY KEY,
+    idEquipoGuia int FOREIGN KEY REFERENCES EquipoGuia(idEquipoGuia),
+    idProfesorGuia varchar(100) FOREIGN KEY REFERENCES Profesor(codigo)
+ 
+);
+
+
+CREATE TABLE Actividad (
+    idActividad iNT IDENTITY (1,1) PRIMARY KEY,
+    semana int,
+    EstadoActividad varchar(100),
+    nombre varchar(100),
+    fechaHora date,
+    fechaAnuncio date,
+    diasPreviosAnuncio int,
+    Modalidad varchar(100),
+    enlaceRemoto varchar(100),
+    afiche varbinary(MAX),
+    TipoActividad varchar(100)
+ 
+  
+);
+
+CREATE TABLE PlanTrabajo
+(
+    idPlanTrabajo INT IDENTITY (1,1) PRIMARY KEY,
+    
+        
+);
+
+CREATE TABLE ActividadXresponsables
+(
+    idActividadXresponsables INT IDENTITY (1,1) PRIMARY KEY,
+    idProfesorGuia VARCHAR(100) FOREIGN KEY REFERENCES Profesor(codigo),
+    idActividad INT FOREIGN KEY REFERENCES Actividad(idActividad)
+
+        
+);
+
+CREATE TABLE Recordatorios
+(
+    idRecordatorios INT IDENTITY (1,1) PRIMARY KEY,
+    idActividad INT FOREIGN KEY REFERENCES Actividad(idActividad),
+    fechaRecordatorio DATE
+       
+);
+
+CREATE TABLE Comentario
+(
+    idComentario INT IDENTITY (1,1) PRIMARY KEY,
+    idProfesorGuia VARCHAR(100) FOREIGN KEY REFERENCES Profesor(codigo),
+    fechaHora DATETIME,
+    cuerpo VARCHAR(100)
+);
+
+CREATE TABLE ActividadXComentario
+(
+    idActividadXComentario INT IDENTITY (1,1) PRIMARY KEY,
+    idActividad INT FOREIGN KEY REFERENCES Actividad(idActividad),
+    idComentario INT FOREIGN KEY REFERENCES Comentario(idComentario)
+        
+);
+
+CREATE TABLE Respuesta
+(
+    idRespuesta INT IDENTITY (1,1) PRIMARY KEY,
+    idProfesorGuia VARCHAR(100) FOREIGN KEY REFERENCES Profesor(codigo),
+    fechaHora DATETIME,
+    cuerpo VARCHAR(100),
+    idComentario INT FOREIGN KEY REFERENCES Comentario(idComentario)
+);
+   
+CREATE TABLE Evidencias
+(
+    idEvidencia INT PRIMARY KEY,
+    idActividad INT,
+    listaAsistencia VARCHAR(max),
+    linkGrabacion VARCHAR(1000)
+        
+);
+
+CREATE TABLE Imagen
+(
+    idImagen INT PRIMARY KEY,
+     idEvidencia INT,
+    Imagen VARbinary(300) 
+);
+
+CREATE TABLE Justificacion
+(
+    idJustificacion int,
+    idActividad int,
+    justificacion VARCHAR(500),
+    fecha datetime
+            
+);
+
+-- CREATE TABLE EvidenciasXimagen
+-- (
+--     idEvidenciasXimagen INT IDENTITY (1,1) PRIMARY KEY,
+--     idImagen INT FOREIGN KEY REFERENCES Imagen(idImagen),
+--    
+--        
+-- );
